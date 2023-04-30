5d2f49dd3d7495641a15de4a3916b831558cf977
ach113 <archieberidze@gmail.com>
2023-04-30 14:46:53 -0700

    add outerProduct, add support for ndarrays in dotProduct

--- src/util.h
+++ src/util.h
@@ -5,7 +5,7 @@ inline __device__ float sigmoid_(float x) { return 1.0 / (1.0 + exp(-x)); }
 __global__ void sigmoid(const float* inputs, float* outputs, const int size);
 __global__ void relu(const float* inputs, float* outputs, const int size);
 __global__ void mul(const float* inputs, float* outputs, const int size, const float scalar);
-__global__ void dotProduct(float *d_a, float *d_b, float *d_result, int n);
-__global__ void outerProduct(float* A, float* B, float* C, int n);
+__global__ void dotProduct(float *A, float *B, float *C, int m, int n, int k);
+__global__ void outerProduct(const float* A, const float* B, float* result, int m, int n);
 __global__ void transpose(const float *input, float *output, int rows, int cols);
 #endif
